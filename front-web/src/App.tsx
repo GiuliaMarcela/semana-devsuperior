d2a64a993d7e7a167f22b5d06d98fdd636f53397
GiuliaMarcela <giuliamendes67@gmail.com>
2020-09-18 10:50:52 -0300

    Criando página de listagem dos registros

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import './App.css';
+import Routes from './Routes';
 
-import Header from './components/Header';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <Routes />
   );
 }
 
